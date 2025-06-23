6e72f8c63b320f9b00550c69a82da19230a89643
Conlin Durbin <conlin.durbin@whatnot.com>
2025-06-23 12:12:13 -0400

    Add port variable

--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,40 @@
-import dotenv from 'dotenv'
-import FeedGenerator from './server'
+import dotenv from "dotenv";
+import FeedGenerator from "./server";
 
 const run = async () => {
-  dotenv.config()
-  const hostname = maybeStr(process.env.FEEDGEN_HOSTNAME) ?? 'example.com'
-  const serviceDid =
-    maybeStr(process.env.FEEDGEN_SERVICE_DID) ?? `did:web:${hostname}`
-  const server = await FeedGenerator.create({
-    port: maybeInt(process.env.FEEDGEN_PORT) ?? 3000,
-    listenhost: maybeStr(process.env.FEEDGEN_LISTENHOST) ?? 'localhost',
-    subscriptionEndpoint:
-      maybeStr(process.env.FEEDGEN_SUBSCRIPTION_ENDPOINT) ??
-      'wss://bsky.network',
-    publisherDid:
-      maybeStr(process.env.FEEDGEN_PUBLISHER_DID) ?? 'did:example:alice',
-    subscriptionReconnectDelay:
-      maybeInt(process.env.FEEDGEN_SUBSCRIPTION_RECONNECT_DELAY) ?? 3000,
-    hostname,
-    serviceDid,
-  })
-  await server.start()
-  console.log(
-    `🤖 running feed generator at http://${server.cfg.listenhost}:${server.cfg.port}`,
-  )
-}
+	dotenv.config();
+	const hostname = maybeStr(process.env.FEEDGEN_HOSTNAME) ?? "example.com";
+	const serviceDid =
+		maybeStr(process.env.FEEDGEN_SERVICE_DID) ?? `did:web:${hostname}`;
+	const server = await FeedGenerator.create({
+		port: maybeInt(process.env.FEEDGEN_PORT ?? process.env.PORT) ?? 3000,
+		listenhost: maybeStr(process.env.FEEDGEN_LISTENHOST) ?? "localhost",
+		subscriptionEndpoint:
+			maybeStr(process.env.FEEDGEN_SUBSCRIPTION_ENDPOINT) ??
+			"wss://bsky.network",
+		publisherDid:
+			maybeStr(process.env.FEEDGEN_PUBLISHER_DID) ?? "did:example:alice",
+		subscriptionReconnectDelay:
+			maybeInt(process.env.FEEDGEN_SUBSCRIPTION_RECONNECT_DELAY) ?? 3000,
+		hostname,
+		serviceDid,
+	});
+	await server.start();
+	console.log(
+		`🤖 running feed generator at http://${server.cfg.listenhost}:${server.cfg.port}`,
+	);
+};
 
 const maybeStr = (val?: string) => {
-  if (!val) return undefined
-  return val
-}
+	if (!val) return undefined;
+	return val;
+};
 
 const maybeInt = (val?: string) => {
-  if (!val) return undefined
-  const int = parseInt(val, 10)
-  if (isNaN(int)) return undefined
-  return int
-}
+	if (!val) return undefined;
+	const int = parseInt(val, 10);
+	if (isNaN(int)) return undefined;
+	return int;
+};
 
-run()
+run();
